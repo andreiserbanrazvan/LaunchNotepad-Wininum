661550299df69efd157c451ba25bc2f6f3ab7dbd
Andrei Serban <andrei.serban@ciel.ro>
2021-01-14 11:18:20 +0200

    Add project files.

---
+++ LaunchNotepad/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("LaunchNotepad")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LaunchNotepad")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("728bef05-1e21-44af-977a-01520a35ef85")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ LaunchNotepad/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Winium;
+using System;
+
+namespace LaunchNotepad
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void LaunchNotepadAndWriteSomething()
+        {
+
+            DesktopOptions options = new DesktopOptions();
+            options.ApplicationPath = "C:\\Windows\\System32\\notepad.exe";
+
+            WiniumDriver driver = new WiniumDriver(@"C:\Users\andrei.serban\Downloads\Winium.Desktop.Driver", options);
+
+        }
+    }
+}
